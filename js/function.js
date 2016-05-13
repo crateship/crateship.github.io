ad2f6219f479320162cabbc618af9e508e99b56a
Romko Pidstryhach <romko.pidstryhach@gmail.com>
2016-05-13 13:21:51 +0300

    Initial commit

---
+++ js/function.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$("a[rel=group1]").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false
+	});
+});
